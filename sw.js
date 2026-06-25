0c0b06f3548c1cde60adcecced07a0ef0f459d93
dqapsys <uv@dqap.in>
2026-06-25 14:51:07 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // DQAP Wiki Service Worker
 // Version 77.4 - compact PMO and Budget layouts
 const CACHE_PREFIX = 'dqap-wiki-';
-const CACHE_VERSION = 'dqap-wiki-v78-20260625-2';
+const CACHE_VERSION = 'dqap-wiki-v78-20260625-3';
 const CACHE_NAME = CACHE_VERSION;
 const APP_SHELL = ['./', './index.html'];
 
